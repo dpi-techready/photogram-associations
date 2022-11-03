2b78c567482b671739aa6624c370d0af98fdc9f5
rixiobarrios <rixiobarrios.consulado@gmail.com>
2022-11-03 14:15:20 +0000

    Added more associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,68 @@ class User < ApplicationRecord
   validates :email, :presence => true
   has_secure_password
 
+  has_many(:received_follow_requests, { :class_name => "FollowRequest", :foreign_key => "recipient_id" })
+
+  has_many(:received_follow_requests, { :class_name => "FollowRequest", :foreign_key => "recipient_id", :dependent => :destroy })
+
+  def received_follow_requests
+    my_id = self.id
+
+    matching_follow_requests = FollowRequest.where({ :recipient_id => my_id })
+
+    return matching_follow_requests
+  end
+
+  has_many(:sent_follow_requests, { :class_name => "FollowRequest", :foreign_key => "sender_id" })
+  
+  has_many(:sent_follow_requests, { :class_name => "FollowRequest", :foreign_key => "sender_id", :dependent => :destroy })
+
+  def sent_follow_requests
+    my_id = self.id
+
+    matching_follow_requests = FollowRequest.where({ :sender_id => my_id })
+
+    return matching_follow_requests
+  end
+
+  has_many(:commented_photos, { :through => :photo_owner, :source => :comments })
+
+  def commented_photos
+    array_of_comment_ids = Array.new
+
+    my_photo_owner = self.photo_owner
+
+    my_photo_owner.each do |a_photo|
+      the_photo_owner_comments = a_photo.comments
+
+      the_photo_owner_comments.each do |a_comment|
+        array_of_comment_ids.push(a_comment.id)
+      end
+    end
+
+    matching_comments = Comment.where({ :id => array_of_comment_ids })
+
+    return matching_comments
+  end
+
+  has_many(:liked_photos, { :through => :user_likes, :source => :photo })
+
+  def liked_photos
+    array_of_photo_ids = Array.new
+
+    my_user_likes = self.user_likes
+
+    my_user_likes.each do |a_like|
+      the_photo = a_like.photo
+
+      array_of_photo_ids.push(the_photo.id)
+  end
+
+  matching_photos = Photo.where({ :id => array_of_photo_ids })
+
+  return matching_photos
+end
+
   has_many(:photo_owner, { :class_name => "Photo", :foreign_key => "owner_id" })
 
   has_many(:photo_owner, { :class_name => "Photo", :foreign_key => "owner_id", :dependent => :destroy })
